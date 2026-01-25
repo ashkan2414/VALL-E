efbed7d633ee2fb1130c278ff3b033f1a14a98ef
Ashkan Ebrahimi <a28ebrah@uwaterloo.ca>
2026-01-24 20:18:49 -0500

    Fix git case-insensitive change

--- firmware/Common/Inc/Valle/Device/device.hpp
+++ firmware/Common/Inc/Valle/Device/device.hpp
@@ -1,4 +1,7 @@
 #pragma once
 
+#include "Valle/Device/devices/adc.hpp"
+#include "Valle/Device/devices/dma.hpp"
+#include "Valle/Device/devices/gpio.hpp"
 #include "Valle/Device/devices/hrtim.hpp"
 #include "Valle/Device/registry.hpp"
--- firmware/Common/Inc/valle/Device/device.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Valle/Device/devices/adc.hpp"
-#include "Valle/Device/devices/dma.hpp"
-#include "Valle/Device/devices/gpio.hpp"
-#include "Valle/Device/devices/hrtim.hpp"
-#include "Valle/Device/registry.hpp"
